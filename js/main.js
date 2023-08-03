aaea45dd35d2ee0fbab3f9ad8c0eea6de9996db8
Federico Luna <federico.luna0912@gmail.com>
2023-08-03 12:53:33 -0300

    tercera pre-entrega

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ let productos = [
     servicios:28
   },
   {
-    id:1,
+    id:2,
     imagen: "../img/true-made.jpg",
     nombreproducto:"Ena Truemade" ,
     descripcion:"Competencia directa de Star y una de las marcas mas reconocidas del pais",
@@ -48,7 +48,7 @@ let productos = [
     servicios:28
   },
   {
-    id:1,
+    id:3,
     imagen: "../img/proteina-nutrilab-1kg1-3f66649d4acd57f02316014302287419-640-0.png",
     nombreproducto:"Nutrilab" ,
     descripcion:"La proteina con mejor relacion Calidad-Precio del mercado argentino",
@@ -57,7 +57,7 @@ let productos = [
     servicios:30
   },
   {
-    id:1,
+    id:4,
     imagen: "../img/whey-chocolate1-b02912dcf3a9a79bdd16521942924693-1024-1024.webp",
     nombreproducto:"Gold" ,
     descripcion:"Una proteina con un excelente aminograma",
@@ -66,7 +66,7 @@ let productos = [
     servicios:30
   },
   {
-    id:1,
+    id:5,
     imagen: "../img/Whey-ADAVANCE.webp",
     nombreproducto:"Body Advance" ,
     descripcion:"Una marca relativamente nueva en el mercado y competencia de Nutrilab",
@@ -75,23 +75,53 @@ let productos = [
     servicios:30
   },
   {
-    id:1,
+    id:6,
     imagen: "../img/BEST-WHEY-Protein-Banana-nuevo-(1).png",
     nombreproducto:"Xtrenght" ,
     descripcion:"La marca insignia del pais nos ofrece esta proteina con una exclenecte relacion precio-calidad",
     precio: 9000,
     contenidoNeto: "1KG",
     servicios: 30
+  },
+  {
+    id:7,
+    imagen: "../img/img1643117240-whey Star Cookies Doy.jpg",
+    nombreproducto:"Platinum Star" ,
+    descripcion:"La marca insignia del pais nos ofrece esta proteina con una exclenecte relacion precio-calidad",
+    precio: 18000,
+    contenidoNeto: "3KG",
+    servicios: 88
+  },
+  {
+    id:8,
+    imagen: "../img/mutant-mass-x-5kg-frutilla.jpg",
+    nombreproducto:"Star Mutant Mass" ,
+    descripcion:"Tipo =Ganador de peso",
+    precio: 20000,
+    contenidoNeto: "5KG",
+    servicios: 120
+  },
+  {
+    id:9,
+    imagen: "../img/descarga.jpeg",
+    nombreproducto:"Platinum Star" ,
+    descripcion:"Tipo = Proteina(aislada)",
+    precio: 12000,
+    contenidoNeto: "1KG",
+    servicios: 30
   }
 ]
 
-let cardBootstrap
+
 let productosCards = document.querySelector(".productosCards")
 let carrito = []
+if (localStorage.getItem("carrito")) {
+  carrito = JSON.parse(localStorage.getItem("carrito"));
+}
 
 for (let i = 0; i < productos.length; i++) {
     let carrito = productos[i]
-    cardBootstrap += `<div class="card" style="width: 18rem;">
+    let cardBootstrap = `<div class="tarjeta-flexbox"> <div class="card" style="width: 18rem;">
     <img src="${carrito.imagen}" class="card-img-top" alt="proteina-star">
     <div class="card-body">
         <h5 class="card-title">${carrito.nombreproducto}</h5>
@@ -100,8 +130,8 @@ for (let i = 0; i < productos.length; i++) {
     <ul class="list-group list-group-flush">
         <li class="list-group-item">$${carrito.precio.toFixed(0)}</li>
         <li class="list-group-item">${carrito.contenidoNeto}</li>
-        <button type="button" class="btn btn-outline-dark" data-producto-id="1">Añadir al carrito</button>
-    </ul>`;
+        <button type="button" class="btn btn-outline-dark" data-producto-id="${carrito.id}">Añadir al carrito</button>
+    </ul> </div>`;
     productosCards.innerHTML += cardBootstrap
   }
 
@@ -114,6 +144,8 @@ for (let i = 0; i < agregarBoton.length; i++) {
 
 function agregarCarrito(event){
   let button = event.target
+  
+  localStorage.setItem("carrito", JSON.stringify(carrito));
   
   let productoId = button.getAttribute("data-producto-id")
   
@@ -131,4 +163,15 @@ function agregarCarrito(event){
   
     console.log("Producto agregado al carrito. ID: " + productoId);
     console.log("Carrito:", carrito);
+}
+
+let botonEliminarCarrito = document.getElementsByClassName("buttonCarritoEliminar");
+
+for (let i = 0; i < botonEliminarCarrito.length; i++) {
+  let eliminar = botonEliminarCarrito[i];
+  eliminar.addEventListener("click", limpiarConsola);
+}
+
+function limpiarConsola() {
+  console.clear();
 }
